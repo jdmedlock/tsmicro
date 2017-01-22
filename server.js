5320ba2525df6998cec76a0ad3eef99d63f42bdf
Jim Medlock <jdmedlock@gmail.com>
2017-01-22 08:49:27 -0600

    completed testing of server.js and index.js

--- server.js
+++ server.js
@@ -20,79 +20,95 @@
 //    { "unix": 1450137600, "natural": "December 15, 2015" }
 
 const express = require("express");
+const moment = require("moment");
+const path = require('path');
 const url = require("url");
 const app = express();
 
 // -------------------------------------------------------------
-// Initialization function(s)
+// Express UI function(s)
 // -------------------------------------------------------------
 
-app.get('/:TIMESTRING', function(request, response) {
-    const query = request.query;
-    let unixTs = new UnixTime();
-    let nlDate = new NlDate();
+app.get("/", function(request, response) {
+    response.sendFile(path.join(__dirname + "/index.html"));
+});
 
-    if (unixTs.isUnixTs(query)) {
-        unixTs = new UnixTime(query);
-        nlDate = unixTs.toNlDate();
-    } else if (nlDate.isNlDate(query)) {
-        nlDate = new NlDate(query);
-        nlDate = nlDate.toUnixTs();
-    }
-    const jsonResult = JSON.stringify({ unix: unixTs, natural: nlDate });;
-    response.send(jsonResult);
+app.get("/:TIMESTRING", function(request, response) {
+    const query = request.params.TIMESTRING;
+    let tsMicroService = new TsMicroService(query);
+
+    const jsonResult = JSON.stringify({
+      unix: tsMicroService.unixTs,
+      natural: tsMicroService.nlDate
+    });
+    response.send(JSON.stringify({
+      unix: tsMicroService.unixTs,
+      natural: tsMicroService.nlDate
+    }));
 });
 
 app.listen(8080, function() {
-    console.log('Example app listening on port 8080!');
+    console.log("Timestamp Microservice app listening on port 8080!");
 });
 
 
 // -------------------------------------------------------------
-// Natural Language Date Class Definition
+// Timestamp Microservice Class Definition
 // -------------------------------------------------------------
-class NlDate {
-    constructor(dateValue) {
-        this.date = dateValue;
+class TsMicroService {
+    // Construct a new TsMicroService object instance. The input
+    // value will be checked to see if it is a Unix timestamp or
+    // a natural language date string. Unique class variables
+    // will be initialized to contain both, with the unknown value
+    // being constructed from the known value.
+    //
+    // For example, if a natural language date is passed the Unix
+    // timestamp will be constructed from it and vice versa.
+    //
+    // If an invalid value is passed both class variables will be
+    // set to null.
+    //
+    // Returns: New object instance
+    constructor(inputValue) {
+        this.nlDate = null;
+        this.unixTs = null;
+
+        if (this.isNlDate(inputValue)) {
+           this.nlDate = inputValue;
+           this.unixTs = this.toUnixTs();
+        } else if (this.isUnixTs(inputValue)) {
+           this.unixTs = Number(inputValue);
+           this.nlDate = this.toNlDate();
+        }
     }
-    // Check a date value to see if it is a natural language date
+    // Check an input value to see if it is a natural language date
     //
     // Returns: true - dateValue is a valid natural language date
     //          false - dateValue is not a natural language date
-    isNlDate(dateValue) {
-        var str='0123456789';
-        return str.match(/^\d{10}$/);
+    isNlDate(inputValue) {
+        const date = moment(inputValue, "MMMM D, YYYY", true);
+        return date.isValid();
+    }
+    // Check an input value to see if it is a Unix timestamp
+    //
+    // Returns: true - timeValue is a valid Unix timestamp
+    //          false - timeValue is not a Unix timestamp
+    isUnixTs(inputValue) {
+      const timestamp = moment(inputValue, "x", true);
+      return timestamp.isValid();
+    }
+    // Convert a Unix timestamp to a natural language date string
+    //
+    // Returns: date - natural language date string
+    toNlDate() {
+        const date = moment(this.unixTs).format("MMMM DD, YYYY");
+        return date;
     }
     // Convert a Unix timestamp to a natural language date string
     //
     // Returns: timeValue - Unix time string
     toUnixTs() {
-        let unixTime = this.date.getMilliseconds();
-        return unixTime;
-    }
-}
-
-// -------------------------------------------------------------
-// Unix Time Class Definition
-// -------------------------------------------------------------
-class UnixTime {
-    constructor(timeValue) {
-        this.time = timeValue;
-    }
-    // Check a time value to see if it is a Unix timestamp
-    //
-    // Returns: true - timeValue is a valid Unix timestamp
-    //          false - timeValue is not a Unix timestamp
-    isUnixTs(timeValue) {
-        var str='0123456789';
-        return str.match(/^\d{10}$/);
-    }
-    // Convert a Unix timestamp to a natural language date string
-    //
-    // Returns: nlDate - natural language date string
-    toNlDate() {
-        let nlDate = new Date(this.time);
-        const formatOptions = {month: 'long', day: '2-digit',  year: 'numeric'};
-        return nlDate.toLocaleDateString("en-US",formatOptions);
+      const timestamp = moment(this.nlDate).format("x");
+      return timestamp;
     }
 }
